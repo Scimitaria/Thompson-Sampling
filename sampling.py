794f13c8677e807151b7657a12dcc8b780c65b9d
Morgan Powers <morganpowers@jessicas-air-3.trinity.local>
2025-04-16 16:30:27 -0500

    complete

--- sampling.py
+++ sampling.py
@@ -16,24 +16,30 @@ for i in range(N):
 
   for j in range(0,d):
     #random draw from beta distribution
-    randomBeta = np.random.beta(nPosReward[j] + 1, nNegReward[j] + 1)
-    if (randomBeta > maxRandom):
+    randomBeta = np.random.beta(nPosReward[j]+1, nNegReward[j]+1)
+    if (randomBeta>maxRandom):
       maxRandom = randomBeta
-      selected = j #best machine
-      timesSelected[selected]+=1
-      #if we won
-      if np.random.rand() < conversionRates[selected]: nPosReward[selected] += 1
-      #if we lost
-      else: nNegReward[selected] += 1
+      selected = j #best machine. Should machine 1 always be gettings selected? Fishy...
+  timesSelected[selected]+=1
+  #if we won
+  if (np.random.rand() < conversionRates[selected]): nPosReward[selected]+=1
+  #if we lost
+  else: nNegReward[selected]+=1
 
 maxP=0
+wins=0
+losses=0
 for m in range(0,d):
-  pos=nPosReward[m]
-  neg=nNegReward[m]
-  best=1
-  p=pos/neg
+  pos = nPosReward[m]
+  wins+=pos
+  neg = nNegReward[m]
+  losses+=neg
+  best = 1
+  p = pos/neg
   if(p>maxP):
-    maxP=p
-    best=m+1
+    maxP = p
+    best = m+1
   print("machine number "+str(m+1)+" was selected "+str(timesSelected[m])+" times ("+str(pos)+" wins, "+str(neg)+" losses, P="+str(p)+")")
 print("Conclusion: the best machine is machine number "+str(best))
+print("total reward: "+str(wins-losses))
+
